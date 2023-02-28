e6b9c4b04e5bed58c857b4d2be22287143bd5733
NicolasLoppin66 <loppin.n66@gmail.com>
2023-02-28 11:27:46 +0100

    Personnalisation des config du projet cloné

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'JavaScript Objet avec WebPack',
+            title: 'JavaScript Objet - Consomation de Webservice',
         })
     ],
     module: {
